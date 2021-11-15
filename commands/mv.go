d23e85dada0a62bcb8e8c347ad5450cf9c14b462
oopjot <pitercpo@gmail.com>
2021-11-15 21:11:57 +0100

    finish mv command

--- commands/mv.go
+++ commands/mv.go
@@ -15,56 +15,114 @@ func Mv(currDir entities.Dir, args ...string) error {
 		return errors.New(fmt.Sprintf("mv: missing destination operand after '%s'", args[0]))
 	}
 
+	var toMove []interface{}
 	last := len(args) - 1
 
-	destParentPath, destName := utils.GetDest(args[last])
-	destParentDir, err := utils.Unpath(destParentPath, currDir)
-	if err != nil {
-		return err
-	}
-
-	for i, objPath := range args[:last] {
-		objParentPath, objName := utils.GetDest(objPath)
-		objParentDir, err := utils.Unpath(objParentPath, currDir)
+	for _, path := range args[:last] {
+		parentPath, name := utils.GetDest(path)
+		parent, err := utils.Unpath(parentPath, currDir)
 		if err != nil {
 			return err
 		}
-		destDir, err := destParentDir.FindDir(destName)
+		dir, err := parent.FindDir(name)
 		if err != nil {
-			if i != 0 {
-				return err
-			}
-			file, err := objParentDir.FindFile(objName)
+			file, err := parent.FindFile(name)
 			if err != nil {
 				return err
+			} else {
+				toMove = append(toMove, file)
 			}
-			destParentDir.Remove(objName)
-			file.Rename(objName)
-			destParentDir.AddFile(file)
-			file.ChangeParent(destParentDir)
+		} else {
+				toMove = append(toMove, dir)
+			}
+	}
+
+	destParentPath, destName := utils.GetDest(args[last])
+	destParent, err := utils.Unpath(destParentPath, currDir)
+	if err != nil {
+		return err
+	}
+	destDir, err := destParent.FindDir(destName)
+	if err != nil {
+		destFile, err := destParent.FindFile(destName)
+		if err != nil {
+			if len(toMove) == 1 {
+				switch toMove[0].(type) {
+					case entities.File:
+						toMove[0].(entities.File).Rename(destName)
+						return nil
+					case entities.Dir:
+						toMove[0].(entities.Dir).Rename(destName)
+						return nil
+					default:
+						return errors.New("mv: unknown error")
+				}
+			}
+			return err
 		}
 
-		file, nil := objParentDir.FindFile(objName)
-		if err != nil {
-			// nie znalazlem pliku
-			dir, err := objParentDir.FindDir(objName)
-			if err != nil {
-				return err
-			}
-			// znalazlem dir
-			err = destDir.AddDir(dir)
-			if err != nil {
-				return err
-			}
-			dir.ChangeParent(destDir)
+		if len(toMove) > 1 {
+			return errors.New(fmt.Sprintf("mv: target '%s' is not a directory", destName))
 		}
-		err = destDir.AddFile(file)
-		if err != nil {
-			destDir.Remove(file.Name())
-			destDir.AddFile(file)
+		switch toMove[0].(type) {
+			case entities.Dir:
+				return errors.New(fmt.Sprintf("mv: target '%s' is not a directory", destName))
+			default:
 		}
-		objParentDir.Remove(objName)
-		file.ChangeParent(destDir)
+		return handleFile(destFile, toMove[0].(entities.File))
+	}
+	return handleDir(destDir, toMove...)
+}
+
+
+func handleDir (dest entities.Dir, source ...interface{}) error {
+	for _, entity := range source {
+		switch entity.(type){
+			case entities.File:
+				fmt.Println("siema plik")
+				name := entity.(entities.File).Name()
+				_, err := dest.FindDir(name)
+				if err == nil {
+					return errors.New(fmt.Sprintf("mv: '%s' already exists"))
+				}
+				_, err = dest.FindFile(name)
+				if err == nil {
+					dest.Remove(name)
+				}
+				entity.(entities.File).Parent().Remove(name)
+				entity.(entities.File).ChangeParent(dest)
+				err = dest.AddFile(entity.(entities.File))
+				if err != nil {
+					return err
+				}
+			case entities.Dir:
+				fmt.Println("Siema dir")
+				name := entity.(entities.Dir).Name()
+				_, err := dest.FindFile(name)
+				if err == nil {
+					return errors.New(fmt.Sprintf("mv: '%s' already exists"))
+				}
+				_, err = dest.FindDir(name)
+				if err == nil {
+					dest.Remove(name)
+				}
+				parent, _ := entity.(entities.Dir).Parent()
+				parent.Remove(name)
+				entity.(entities.Dir).ChangeParent(dest)
+				dest.AddDir(entity.(entities.Dir))
+			default:
+				return errors.New("mv: unknown error")
+		}
 	}
 	return nil
 }
+
+func handleFile (dest entities.File, source entities.File) error {
+	name := source.Name()
+	source.Parent().Remove(name)
+	source.ChangeParent(dest.Parent())
+	dest.Parent().Remove(name)
+	dest.Parent().AddFile(source)
+	return nil
+}
+
